73f9722812e478c26f475b99624bf40db9b860f5
Thom Strub <thomstrub@gmail.com>
2022-09-12 12:16:08 -0700

    updates python syntax

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 print("hello world") 
 
-count = 3
+numbers = [1, 2, 3, 4]
+sum = 0
 
-for num in count:
-    print("The count is " + num)
+for num in numbers:
+    print("The sum is " + str(num + sum))
+    sum += num
